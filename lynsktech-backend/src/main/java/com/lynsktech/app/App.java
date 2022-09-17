190c7ae91394641eed2f44651033693a4f1fd8ca
Gregory Lynskey <greglynskey@Gregorys-MacBook-Air-2.local>
2022-09-17 14:12:46 -0400

    Added the maven project

---
+++ lynsktech-backend/src/main/java/com/lynsktech/app/App.java
@@ -0,0 +1,13 @@
+package com.lynsktech.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
