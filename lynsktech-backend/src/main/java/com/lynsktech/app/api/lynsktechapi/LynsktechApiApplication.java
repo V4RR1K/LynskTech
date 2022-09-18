98e70fbffa71ca079139d111b647bd02094fd30a
Gregory Lynskey <greglynskey@Gregorys-MacBook-Air-2.local>
2022-09-17 23:43:29 -0400

    Updating pom.xml

---
+++ lynsktech-backend/src/main/java/com/lynsktech/app/api/lynsktechapi/LynsktechApiApplication.java
@@ -0,0 +1,11 @@
+package com.lynsktech.app.api.lynsktechapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LynsktechApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LynsktechApiApplication.class, args);
+    }
+}
