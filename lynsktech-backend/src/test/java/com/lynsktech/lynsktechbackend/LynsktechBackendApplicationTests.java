4321f65ac27f143f3ba66408f9235e83fef37697
Greg Lynskey <gclynskey@outlook.com>
2022-11-23 16:02:44 -0500

    Refactored backend boilerplate code, should avoid any pom.xml errors now

---
+++ lynsktech-backend/src/test/java/com/lynsktech/lynsktechbackend/LynsktechBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lynsktech.lynsktechbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LynsktechBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
