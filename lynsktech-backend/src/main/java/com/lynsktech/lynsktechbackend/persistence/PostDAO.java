4b52f43a53e1b406cb85d5244e639e17041cfdfe
Greg Lynskey <gclynskey@outlook.com>
2023-02-28 22:39:53 -0500

    Greg: Cleaned up and documented data access interface

--- lynsktech-backend/src/main/java/com/lynsktech/lynsktechbackend/persistence/PostDAO.java
+++ lynsktech-backend/src/main/java/com/lynsktech/lynsktechbackend/persistence/PostDAO.java
@@ -8,15 +8,65 @@ import com.Lynsktech.lynsktechbackend.model.Post;
  */
 public interface PostDAO {
 
+    /**
+     * Retrieves all post objects
+     *
+     * @return An array of post objects
+     *
+     * @throws IOException if issue with storage
+     */
     Post[] getPosts() throws IOException;
 
+    /**
+     * Retrieves all post objects with specific title
+     *
+     * @return An array of post objects
+     *
+     * @throws IOException if issue with storage
+     */
     Post[] findPostsByTitle(String containsText) throws IOException;
 
+    /**
+     * Retrieves post by id
+     *
+     * @param id of post being retrieved
+     *
+     * @return singular post object
+     *
+     * @throws IOException if issue with storage
+     */
     Post getPost(int id) throws IOException;
 
+    /**
+     * Creates a new post object
+     *
+     * @param newPost new post object
+     *
+     * @return post object created
+     *
+     * @throws IOException if issue with storage
+     */
     Post createPost(Post newPost) throws IOException;
 
+    /**
+     * Updates existing post object
+     *
+     * @param updatedPost post being updated
+     *
+     * @return updated post object
+     *
+     * @throws IOException if issue with storage
+     */
     Post updatePost(Post updatedPost) throws IOException;
 
-    Post deletePost(int id) throws IOException;
+    /**
+     * Deletes post given id
+     *
+     * @param id of post being deleted
+     *
+     * @return true if deleted, false if not
+     *
+     * @throws IOException if issue with storage
+     */
+    boolean deletePost(int id) throws IOException;
 }
